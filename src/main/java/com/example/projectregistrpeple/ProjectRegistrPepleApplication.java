59f35bac598cad1f14538a72c8210f354a3e505a
papuk <oleg.papuk@privatbank.ua>
2022-03-30 14:45:42 +0300

    connect consul and connect liquibase

---
+++ src/main/java/com/example/projectregistrpeple/ProjectRegistrPepleApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectregistrpeple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRegistrPepleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectRegistrPepleApplication.class, args);
+    }
+
+}
