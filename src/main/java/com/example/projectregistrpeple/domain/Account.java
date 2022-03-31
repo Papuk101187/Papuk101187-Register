4c904d1c80c598e636b1e6567f0ec3a573c92b8d
papuk <oleg.papuk@privatbank.ua>
2022-03-31 17:16:07 +0300

    fiks

---
+++ src/main/java/com/example/projectregistrpeple/domain/Account.java
@@ -0,0 +1,12 @@
+package com.example.projectregistrpeple.domain;
+
+public class Account {
+
+
+    String login;
+    String password;
+    String access;
+    String names_users;
+
+
+}
--- src/main/java/com/example/projectregistrpeple/service/userservice/UserService.java
+++ src/main/java/com/example/projectregistrpeple/service/userservice/UserService.java
@@ -3,16 +3,20 @@ package com.example.projectregistrpeple.service.userservice;
 
 import com.example.projectregistrpeple.domain.User;
 import com.example.projectregistrpeple.dto.ResponseUser;
+import com.example.projectregistrpeple.patterns.FactoryUsers;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
-public class UserService implements UserServiceInterface{
+public class UserService implements UserServiceInterface {
+
+    FactoryUsers factoryUsers = new FactoryUsers();
 
 
     @Override
     public void AddUser(ResponseUser responseUser, Date date) {
+
 
     }
 
